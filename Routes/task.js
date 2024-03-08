7633f795df5d6a46eb49253b385c7b55a028ed09
vijipriya110 <vijipriya110@gmail.com>
2024-03-08 11:26:22 +0530

    done

--- Routes/task.js
+++ Routes/task.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 
 //to get students data
+
 router.get("/students-all", async(req, res)=>{
     try {
         const data = await Student.find()
@@ -20,6 +21,7 @@ router.get("/students-all", async(req, res)=>{
 })
 
 //to get mentors data
+
 router.get("/mentors-all", async(req, res)=>{
     try {
         const data = await Mentor.find()
@@ -30,7 +32,8 @@ router.get("/mentors-all", async(req, res)=>{
         return res.status(500).json({data:"Internl server error"})
     }
 })
-//to create a student
+//API for create student
+
 router.post("/add-stud", async(req, res)=>{
     try {
         const { name } = req.body;
@@ -45,7 +48,8 @@ router.post("/add-stud", async(req, res)=>{
 
     }
 })
-//to create a mentor
+//API for create Mentor
+
 router.post("/add-ment", async(req, res)=>{
     try {
         const { name } = req.body;
@@ -60,60 +64,58 @@ router.post("/add-ment", async(req, res)=>{
 
     }
 })
-//Assign student to mentor
+//API to Assign a student to Mentor
+
 router.post("/assign/:mentorId/:studentId", async(req, res)=>{
     try {
         const { mentorId, studentId } = req.params;
+        //find student by id
         const student = await Student.findById(studentId)
         if(!student.currentMentor){
+            //find mentor by id
             const mentor = await Mentor.findById(mentorId)
-           if(mentor){
-            Student.findByIdAndUpdate(studentId, { currentMentor: mentorId });
+           //one mentor and Add multiple Student 
+            await Student.findByIdAndUpdate(studentId, { currentMentor: mentorId });
             return res.status(200).json({ Message: "Student assigned to mentor successfully" });
-            
-           }
-                     
+                    
         }else{
             res.status(400).json({ Message: "Already, Student assigned to mentor" });
         }
-        
-        
         
     } catch (error) {
         console.log(error)
         return res.status(500).json({data:"Internal server error"})
     }
-        
-    
+     
 })
 
-//reassign to 
+//API to Assign or Change Mentor for particular Student
+
 router.post("/reassign/:mentorId/:studentId", async(req, res)=>{
     try {
-        const { mentorId, studentId } = req.params;
-        const mentor = await Mentor.findById(mentorId)
-        if(mentor){
+           const { mentorId, studentId } = req.params;
+    
+            //find student
             const student = await Student.findById(studentId)
-            if (student.currentMentor){
-                if (!student.previousMentor.includes(student.currentMentor)) {
-                    let previousmentor = [...student.previousMentor, student.currentMentor];
-                    Student.findByIdAndUpdate(studentId, { currentMentor: mentorId, previousMentor: previousmentor });
-                    return res.status(200).json({ Message: "Mentor reassigned to student successfully" });
-                } else {
-                    Student.findByIdAndUpdate(studentId, { currentMentor: mentorId });
-                    return res.status(200).json({ Message: "Student assigned to mentor successfully" });
-                }
-            }else {
-                res.status(400).json({ Message: "Mentor not found" })
+            if(!student.currentMentor){
+                return res.status(400).json({message:"first need to Assign mentor"})
             }
+            //find mentor
+            const mentor = await Mentor.findById(mentorId)
             
-        }
+            //find and update the current mentor
+            let previousmentor = [...student.previousMentor, student.currentMentor];
+            await Student.findByIdAndUpdate(studentId, { currentMentor: mentorId, previousMentor: previousmentor });
+            return res.status(200).json({ Message: "Mentor reassigned to student successfully"});
+            
     } catch (error) {
         console.log(error)
         return res.status(500).json({data:"Internl server error"})
     }
 })
+
 //Get all students for a particular mentor
+
 router.get("/mentor/:mentorId", async(req, res)=>{
     try {
         const { mentorId } = req.params;
@@ -127,6 +129,7 @@ router.get("/mentor/:mentorId", async(req, res)=>{
 
 
 //Get the previously assigned mentor for a particular student
+
 router.get("/student/:studentId",async (req,res)=>{
     try {
         const { studentId } = req.params;
@@ -142,4 +145,5 @@ router.get("/student/:studentId",async (req,res)=>{
 
     }
 })
+
 export const taskRouter = router
